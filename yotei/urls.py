93b2bae2667d628d4e1779b487e34f0ef264e164
aaruff <anwarruff@gmail.com>
2019-09-30 09:24:40 -0400

    added login features, and user test dir

--- yotei/urls.py
+++ yotei/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
