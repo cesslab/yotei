6832959a138339405430e68e8b21d268971e4eff
aaruff <anwarruff@gmail.com>
2019-10-05 18:40:12 -0400

    added role to user

--- users/models.py
+++ users/models.py
@@ -3,5 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    MANAGER = 1
+    EXPERIMENTER = 2
+    SUBJECT = 3
+    ROLE = ((MANAGER, 'MANAGER'), (EXPERIMENTER, 'EXPERIMENTER'), (SUBJECT, 'SUBJECT'))
+
     full_name = models.CharField('full name', max_length=255)
+    role = models.PositiveSmallIntegerField('role', choices=ROLE)
 
