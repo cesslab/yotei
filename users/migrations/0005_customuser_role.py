7e1d7dadda1a10e6afd3261fa0a883bb506d846f
aaruff <anwarruff@gmail.com>
2019-10-06 18:39:33 -0400

    added role field to the user model

--- users/migrations/0005_customuser_role.py
+++ users/migrations/0005_customuser_role.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customuser',
             name='role',
-            field=models.PositiveSmallIntegerField(choices=[(1, 'MANAGER'), (2, 'EXPERIMENTER'), (3, 'SUBJECT')], default=1, verbose_name='role'),
+            field=models.PositiveSmallIntegerField(choices=[(1, 'MANAGER'), (2, 'RESEARCHER'), (3, 'SUBJECT')], default=1, verbose_name='role'),
             preserve_default=False,
         ),
     ]
