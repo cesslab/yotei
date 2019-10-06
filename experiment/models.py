20000b25cb3f4e8a9e709ce6255cd5f1eead5efa
aaruff <anwarruff@gmail.com>
2019-10-06 18:37:51 -0400

    added experiment, session and session list models

--- experiment/models.py
+++ experiment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Researcher, Subject
+
+
+class Experiment(models.Model):
+    name = models.CharField(max_length=255)
+    researchers = models.ManyToManyField(Researcher)
+
+
+class Session(models.Model):
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+
+
+class SessionList(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subject)
