d23671ee594a3cda2967835596f659f92a0b5133
aaruff <anwarruff@gmail.com>
2019-10-06 18:40:25 -0400

    added role models and the role field to the user model

--- users/models.py
+++ users/models.py
@@ -4,10 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     MANAGER = 1
-    EXPERIMENTER = 2
+    RESEARCHER = 2
     SUBJECT = 3
-    ROLE = ((MANAGER, 'MANAGER'), (EXPERIMENTER, 'EXPERIMENTER'), (SUBJECT, 'SUBJECT'))
+    ROLE = ((MANAGER, 'MANAGER'), (RESEARCHER, 'RESEARCHER'), (SUBJECT, 'SUBJECT'))
 
     full_name = models.CharField('full name', max_length=255)
     role = models.PositiveSmallIntegerField('role', choices=ROLE)
 
+
+class Researcher(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+class Manager(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+
+class Subject(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
