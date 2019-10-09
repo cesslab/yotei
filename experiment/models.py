72944ccca394750c622a89947d2cbd77be7e0dcd
aaruff <anwarruff@gmail.com>
2019-10-09 09:59:25 -0400

    add

--- experiment/models.py
+++ experiment/models.py
@@ -6,6 +6,7 @@ from users.models import Researcher, Subject
 class Experiment(models.Model):
     name = models.CharField(max_length=255)
     researchers = models.ManyToManyField(Researcher)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -13,8 +14,29 @@ class Experiment(models.Model):
 
 class Session(models.Model):
     experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    # 12052019_12:30pm_1
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}_{}_{}".format(self.datetime.date(), self.datetime.time(), self.pk)
 
 
-class SessionList(models.Model):
+class PrimaryList(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.session
+
+
+class WaitList(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return self.session
+
+
+
